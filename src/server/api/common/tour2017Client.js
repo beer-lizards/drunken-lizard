ad6be6a1b6e8e95ec545973e5349e0b96080f18a
Cameron Rollhieser <cameron.rollheiser@heurekasoftware.com>
2017-03-22 00:58:04 -0400

    Tour 2017 Routes
    Added api calls to fetch card history, user details, and gifts.

---
+++ src/server/api/common/tour2017Client.js
@@ -0,0 +1,326 @@
+import bunyan from 'bunyan';
+import crypto from 'crypto';
+import Promise from 'bluebird';
+import request from 'request';
+import url from 'url';
+
+import config from '../../config';
+
+const log = bunyan.createLogger({ name: 'lizard' });
+const { host, port, softwareId } = config.winkingLizard2017;
+
+/**
+ * Fetches the current beer list and consumed beers from the api.
+ */
+export function fetchBeerHistory({
+  email,
+  password,
+}) {
+  log.info('tour2017 - fetchBeerHistory - start');
+  return new Promise((resolve, reject) => {
+    const validation = {
+      fields: [],
+    };
+    if (!email) {
+      validation.fields.push('email');
+    }
+
+    if (!password) {
+      validation.fields.push('password');
+    }
+
+    if (validation.fields.length > 0) {
+      return reject(validation);
+    }
+
+    // Verify credential.
+    const hash = crypto.createHash('md5');
+    hash.update(`${password}`);
+
+    const path = url.format({
+      protocol: 'http',
+      hostname: host,
+      port,
+      pathname: '_110/VIGuestSignUp/CFGet/VIGuestAccounts.cfc',
+      query: {
+        method: 'VIGItemHistory',
+        UserID: email,
+        Password: hash.digest('hex'),
+        SoftwareID: softwareId,
+        StoreSerial: 80216,
+        StoreID: 5420,
+      },
+    });
+
+    const opts = {
+      method: 'GET',
+      url: path,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    return request(opts, (err, response, body) => {
+      if (err || response.statusCode < 200 || response.statusCode > 300) {
+        log.info('tour2017 - fetchBeerHistory - err');
+        return reject(err);
+      }
+      log.info('tour2017 - fetchBeerHistory - success');
+      return resolve(JSON.parse(body));
+    });
+  });
+}
+
+/**
+ * Fetches the current card history.
+ */
+export function fetchCardHistory({
+  email,
+  password,
+}) {
+  log.info('tour2017 - fetchCardHistory - start');
+  return new Promise((resolve, reject) => {
+    const validation = {
+      fields: [],
+    };
+    if (!email) {
+      validation.fields.push('email');
+    }
+
+    if (!password) {
+      validation.fields.push('password');
+    }
+
+    if (validation.fields.length > 0) {
+      return reject(validation);
+    }
+
+    // Verify credential.
+    const hash = crypto.createHash('md5');
+    hash.update(`${password}`);
+
+    const path = url.format({
+      protocol: 'http',
+      hostname: host,
+      port,
+      pathname: '_110/VIGuestSignUp/CFGet/VIGuestAccounts.cfc',
+      query: {
+        method: 'VIGCardHistory',
+        UserID: email,
+        Password: hash.digest('hex'),
+        SoftwareID: softwareId,
+        StoreSerial: 80216,
+        StoreID: 5420,
+      },
+    });
+
+    const opts = {
+      method: 'GET',
+      url: path,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    return request(opts, (err, response, body) => {
+      if (err || response.statusCode < 200 || response.statusCode > 300) {
+        log.info('tour2017 - fetchCardHistory - err');
+        return reject({
+          message: 'Invalid credentials.',
+        });
+      }
+      log.info('tour2017 - fetchCardHistory - success');
+      return resolve(JSON.parse(body));
+    });
+  });
+}
+
+/**
+ * Fetches a users gift history.
+ */
+export function fetchGiftHistory({
+  email,
+  password,
+}) {
+  log.info('tour2017 - fetchGiftHistory - start');
+  return new Promise((resolve, reject) => {
+    const validation = {
+      fields: [],
+    };
+    if (!email) {
+      validation.fields.push('email');
+    }
+
+    if (!password) {
+      validation.fields.push('password');
+    }
+
+    if (validation.fields.length > 0) {
+      return reject(validation);
+    }
+
+    // Verify credential.
+    const hash = crypto.createHash('md5');
+    hash.update(`${password}`);
+
+    const path = url.format({
+      protocol: 'http',
+      hostname: host,
+      port,
+      pathname: '_110/VIGuestSignUp/CFGet/VIGuestAccounts.cfc',
+      query: {
+        method: 'GetAvailableVoucher',
+        UserID: email,
+        Password: hash.digest('hex'),
+        SoftwareID: softwareId,
+        StoreSerial: 80216,
+        StoreID: 5420,
+      },
+    });
+
+    const opts = {
+      method: 'GET',
+      url: path,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    return request(opts, (err, response, body) => {
+      if (err || response.statusCode < 200 || response.statusCode > 300) {
+        log.info('tour2017 - fetchGiftHistory - err');
+        return reject({
+          message: 'Invalid credentials.',
+        });
+      }
+      log.info('tour2017 - fetchGiftHistory - success');
+      return resolve(JSON.parse(body));
+    });
+  });
+}
+
+/**
+ * Fetch the user details.
+ */
+export function fetchUser({
+  email,
+  password,
+}) {
+  log.info('tour2017 - fetchUser - start');
+  return new Promise((resolve, reject) => {
+    const validation = {
+      fields: [],
+    };
+    if (!email) {
+      validation.fields.push('email');
+    }
+
+    if (!password) {
+      validation.fields.push('password');
+    }
+
+    if (validation.fields.length > 0) {
+      return reject(validation);
+    }
+
+    // Verify credential.
+    const hash = crypto.createHash('md5');
+    hash.update(`${password}`);
+
+    const path = url.format({
+      protocol: 'http',
+      hostname: host,
+      port,
+      pathname: '_110/VIGuestSignUp/CFGet/VIGuestAccounts.cfc',
+      query: {
+        method: 'GetVIGuest',
+        UserID: email,
+        Password: hash.digest('hex'),
+        SoftwareID: softwareId,
+      },
+    });
+
+    const opts = {
+      method: 'GET',
+      url: path,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    return request(opts, (err, response, body) => {
+      if (err || response.statusCode < 200 || response.statusCode > 300) {
+        log.info('tour2017 - fetchUser - err');
+        return reject({
+          message: 'Invalid credentials.',
+        });
+      }
+      log.info('tour2017 - fetchUser - success');
+      return resolve(JSON.parse(body));
+    });
+  });
+}
+
+/**
+ * Verifies the users credential.
+ */
+export function verifyCredentials({
+  email,
+  password,
+}) {
+  log.info('tour2017 - authenticatePromise - start');
+  return new Promise((resolve, reject) => {
+    const validation = {
+      fields: [],
+    };
+    if (!email) {
+      validation.fields.push('email');
+    }
+
+    if (!password) {
+      validation.fields.push('password');
+    }
+
+    if (validation.fields.length > 0) {
+      return reject(validation);
+    }
+
+    // Verify credential.
+    const hash = crypto.createHash('md5');
+    hash.update(`${password}`);
+
+    const path = url.format({
+      protocol: 'http',
+      hostname: host,
+      port,
+      pathname: '_110/VIGuestSignUp/CFGet/VIGuestAccounts.cfc',
+      query: {
+        method: 'VIGLoginVerification',
+        UserID: email,
+        Password: hash.digest('hex'),
+      },
+    });
+
+    const opts = {
+      method: 'GET',
+      url: path,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    return request(opts, (err, response) => {
+      if (err || response.statusCode < 200 || response.statusCode > 300) {
+        log.info('tour2017 - authenticatePromise - err');
+        return reject({
+          message: 'Invalid credentials.',
+        });
+      }
+      log.info('tour2017 - authenticatePromise - success');
+      return resolve({
+        email,
+        password,
+      });
+    });
+  });
+}
--- src/server/api/tour2017/sessions.js
+++ src/server/api/tour2017/sessions.js
@@ -1,79 +1,30 @@
 import bunyan from 'bunyan';
-import crypto from 'crypto';
 import express from 'express';
 import moment from 'moment';
 import Promise from 'bluebird';
-import request from 'request';
-import url from 'url';
 
-import config from '../../config';
+import * as tour2017Client from '../common/tour2017Client';
 
 const log = bunyan.createLogger({ name: 'lizard' });
 
 const router = express.Router();
-const { host, port } = config.winkingLizard2017;
 
 const storeCredentials = (req, res) => {
   log.info('tour2017 - sessions - POST');
-  return new Promise((resolve, reject) => {
-    const {
-      email,
-      password,
-    } = req.body;
-
-    const validation = {
-      fields: [],
-    };
-    if (!email) {
-      validation.fields.push('email');
-    }
-
-    if (!password) {
-      validation.fields.push('password');
-    }
-
-    if (validation.fields.length > 0) {
-      return res.json(validation);
-    }
-
-    // Verify credential.
-    const hash = crypto.createHash('md5');
-    hash.update(`${password}`);
-
-    const path = url.format({
-      protocol: 'http',
-      hostname: host,
-      port,
-      pathname: '_110/VIGuestSignUp/CFGet/VIGuestAccounts.cfc',
-      query: {
-        method: 'VIGLoginVerification',
-        UserID: email,
-        Password: hash.digest('hex'),
-      },
-    });
-
-    const opts = {
-      method: 'GET',
-      url: path,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    };
-    return request(opts, (error, response) => {
-      if (!error && response.statusCode === 200) {
-        return resolve({
-          email,
-          password,
-        });
-      }
-      return reject('Invalid credentials.');
-    });
-  }).then((verifiedCredentials) => {
+  const {
+    email,
+    password,
+  } = req.body;
+  return tour2017Client.verifyCredentials({
+    email,
+    password,
+  })
+  .then((verifiedCredentials) => {
     return new Promise((resolve) => {
-      const { id } = req.params;
+      const { userId } = req.params;
       const db = req.app.get('db');
       db.drunken_lizard.winking_lizard.save({
-        user_account_id: id,
+        user_account_id: userId,
         tour_year: 2017,
         credentials: verifiedCredentials,
         password_rotation: false,
@@ -88,14 +39,16 @@ const storeCredentials = (req, res) => {
         resolve(inserted);
       });
     });
-  }).then((inserted) => {
+  })
+  .then((inserted) => {
     res.json(inserted);
-  }).catch((err) => {
+  })
+  .catch((err) => {
     res.status(400).json(err);
   });
 };
 
-router.route('/:id')
+router.route('/:userId')
   .all((req, res, next) => {
     // Verify authorization.
     log.trace('verify - jwt');
