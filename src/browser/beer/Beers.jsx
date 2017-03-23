61e6ffab7e293c27fb925b4d4a7e0beae7aca952
Cameron Rollheiser <icirellik@gmail.com>
2017-03-22 22:28:00 -0400

    Page Cleaning
    Just cleaning up several pages and doing some housekeeping.

--- src/browser/beer/Beers.jsx
+++ src/browser/beer/Beers.jsx
@@ -1,20 +1,10 @@
-import React, { PropTypes } from 'react';
-import {
-  Table,
-  TableBody,
-  TableHeader,
-  TableHeaderColumn,
-  TableRow,
- } from 'material-ui/Table';
-import {
-   Card,
-   CardActions,
-   CardHeader,
-   CardText,
-} from 'material-ui/Card';
-import Toggle from 'material-ui/Toggle';
-import Paper from 'material-ui/Paper';
+import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card';
 import { grey200 } from 'material-ui/styles/colors';
+import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow } from 'material-ui/Table';
+import IconButton from 'material-ui/IconButton';
+import Paper from 'material-ui/Paper';
+import React, { PropTypes } from 'react';
+import Toggle from 'material-ui/Toggle';
 
 import Beer from './Beer';
 
@@ -42,6 +32,9 @@ class Beers extends React.Component {
 
   render() {
     const { beers, showAll } = this.props.beers;
+
+    const page = 0;
+    const total = 0;
 
     if (!beers.length) {
       return (
@@ -92,6 +85,7 @@ class Beers extends React.Component {
                 showRowHover
               >
                 {beers.filter(beer => (showAll) ? true : beer.consumed)
+                  .slice(0, 10)
                   .map(beer => (
                     <Beer
                       key={beer.beerId}
@@ -102,6 +96,31 @@ class Beers extends React.Component {
               </TableBody>
             </Table>
           </Paper>
+          <CardActions style={{ textAlign: 'right' }}>
+            <div
+              style={{
+                display: 'inline-block',
+                height: '48px',
+                lineHeight: '48px',
+                verticalAlign: 'top' }}
+            >
+              {`${page} - ${total}`}
+            </div>
+            <IconButton
+              onTouchTap={() => this.onPrevious()}
+              iconClassName="material-icons"
+              tooltip="Previous page"
+            >
+              navigate_before
+            </IconButton>
+            <IconButton
+              onTouchTap={() => this.onNext()}
+              iconClassName="material-icons"
+              tooltip="Next page"
+            >
+              navigate_next
+            </IconButton>
+          </CardActions>
         </CardText>
       </Card>
     );
